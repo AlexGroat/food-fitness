da3df9f273a142cfad42388bda0cfae49228e668
Alex Groat <alex.groat.95@gmail.com>
2021-10-19 15:31:28 +1030

    Added search button for meals can copy and change info for exercise

--- Assets/js/meals.js
+++ Assets/js/meals.js
@@ -2,3 +2,18 @@
 
 var foodApiKey = "927a07e8df4c43d9a987c5a1a7ed9584";
 
+mealUrl = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' +foodApiKey;
+
+var mealContainer = document.querySelector("meal-list")
+
+
+function searchMeal (query) {
+    const Url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=927a07e8df4c43d9a987c5a1a7ed9584&query=" + $(query);
+    fetch (Url)
+    .then(response => response.json())
+    .then((jsonData) => {
+        console.log("we got meal data");
+        console.log(jsonData);
+    });
+
+}
