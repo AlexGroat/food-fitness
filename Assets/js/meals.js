69d22e1b5599e00859f07127593af58af5cb5c7d
Alex Groat <alex.groat.95@gmail.com>
2021-10-19 20:00:01 +1030

    working query function

--- Assets/js/meals.js
+++ Assets/js/meals.js
@@ -1,21 +1,40 @@
 // spoontacular APIkey
+var searchedMeals = document.querySelector("#appended_meals");
+
+var mealButton = document.querySelector(".search-meals");
 
 var foodApiKey = "927a07e8df4c43d9a987c5a1a7ed9584";
 
-mealUrl = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' +foodApiKey;
+mealUrl = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + foodApiKey;
 
 var mealContainer = document.querySelector("meal-list")
 
 searchedMealArray = [];
 
-function searchMeal (query) {
-    const Url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=927a07e8df4c43d9a987c5a1a7ed9584&query=" + $[query] + "&number=50&offset=5";
-    fetch (Url)
-    .then(response => response.json())
-    .then((jsonData) => {
-    console.log(jsonData);
-    
-       
-    });
+function searchMeal(event) {
+    event.preventDefault();
+    var queryMeals = document.querySelector("#meal_search");
 
+
+    const Url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=927a07e8df4c43d9a987c5a1a7ed9584&query=${queryMeals.value}&number=50`;
+    fetch(Url)
+        .then(response => response.json())
+        .then((jsonData) => {
+            console.log(jsonData);
+        });
 }
+
+var mealName = document.createElement("h1");
+var mealImg =  document.createElement("img");
+
+
+
+
+
+
+
+
+mealButton.addEventListener("submit", searchMeal);
+
+
+
