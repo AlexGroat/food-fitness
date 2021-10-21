fad86f1d7951caec35eab44ade74ca028109ae29
Kobe Robinson <koberobinson02@gmail.com>
2021-10-21 21:05:03 +1000

    idk

--- Assets/js/login.js
+++ Assets/js/login.js
@@ -1,22 +1,5 @@
 var user = JSON.parse(localStorage.getItem('user'));
 
-<<<<<<< HEAD
-const login = document.getElementById("form_id")
-const loginButton = document.getElementById("login")
-
-
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const user.email = form_id.username
-    const user.password = form_id.password
-
-    if (user.email === localStorage.getItem("email") && user.password === localStorage.getItem("password")) {
-        console.log("correct things")
-    } else {
-        console.log("incorrect things")
-    }
-})
-=======
 var emailInput = document.querySelector("#email");
 var passwordInput = document.querySelector("#password");
 var loginButton = document.querySelector(".login-form");
@@ -35,11 +18,3 @@ function userLogin(event) {
 }
 
 loginButton.addEventListener("submit", userLogin);
-
-
-
-// click event listener
-// read value of email
-// read value of password
-// compare value
->>>>>>> 8f41cdf10cb4bdfc02d2c2e793109a34ef2f1043
