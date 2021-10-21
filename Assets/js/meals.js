a9b1ddb14844ea898169ba1775ff23e71b96b9b1
Alex Groat <alex.groat.95@gmail.com>
2021-10-21 15:05:51 +1030

    fixed meal js bug to work properly

--- Assets/js/meals.js
+++ Assets/js/meals.js
@@ -1,55 +1,34 @@
 // spoontacular APIkey
 var searchedMeals = document.querySelector("#appended_meals");
-
 var mealButton = document.querySelector(".search-meals");
-
 var foodApiKey = "f10bded0f0fb4c758b590774eff56541";
-
-mealUrl = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + foodApiKey;
 
 var mealContainer = document.querySelector("meal-list")
 
-searchedMealArray = [];
 function searchMeal(event) {
     event.preventDefault();
     var queryMeals = document.querySelector("#meal_search");
-
-
-    const Url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=f10bded0f0fb4c758b590774eff56541&query${queryMeals.value}&number=50`;
+    const Url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${foodApiKey}&query=${queryMeals.value}&number=50`;
     fetch(Url)
         .then(response => response.json())
         .then((jsonData) => {
             searchedMeals.innerHTML = "";
             for (var i = 0; i < jsonData.results.length; i++) {
-                var mealAppendDiv = document.createElement("div");
+                var appendDiv = document.createElement("div");
                 var mealName = document.createElement("h1");
                 var mealImg = document.createElement("img");
-    
+
                 mealImg.setAttribute("src", jsonData.results[i].image);
                 mealName.textContent = jsonData.results[i].title;
-    
-                mealAppendDiv.appendChild(mealImg);
-                mealAppendDiv.appendChild(mealName);
-                searchedMeals.appendChild(mealAppendDiv);
+
+                appendDiv.appendChild(mealImg);
+                appendDiv.appendChild(mealName);
+                searchedMeals.appendChild(appendDiv);
 
             }
-         
+
             console.log(jsonData);
         });
 }
 
-
-
-
-
-
-
-
-
-
-
-
 mealButton.addEventListener("submit", searchMeal);
-
-
-
