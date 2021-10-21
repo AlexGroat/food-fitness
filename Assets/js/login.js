2d161a3afec94b07eb4ccd451d653348c617b09e
Kobe Robinson <koberobinson02@gmail.com>
2021-10-21 20:55:29 +1000

    fortyfour

--- Assets/js/login.js
+++ Assets/js/login.js
@@ -1,4 +1,17 @@
 var user =JSON.parse(localStorage.getItem('user'));
 
+const login = document.getElementById("form_id")
+const loginButton = document.getElementById("login")
 
-console.log(user);
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const user.email = form_id.username
+    const user.password = form_id.password
+
+    if (user.email === localStorage.getItem("email") && user.password === localStorage.getItem("password")) {
+        console.log("correct things")
+    } else {
+        console.log("incorrect things")
+    }
+})
